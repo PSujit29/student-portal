edc49bd6aa399802be219edf3145a452e4b1c901
PSujit29 <iamsujeet29@gmail.com>
2025-12-24 14:14:12 +0545

    refactor: remove FrontController and associated front router

--- src/router/front.router.js
+++
@@ -1,9 +0,0 @@
-const frontRouter = require("express").Router()
-const FrontController = require("../controllers/front.controller")
-
-const frontCtrl = new FrontController
-
-frontRouter.get("/", frontCtrl.homeRequest)
-frontRouter.post("/", frontCtrl.handleHomeRequest)
-
-module.exports = frontRouter
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,10 @@
 const router = require("express").Router()
 
-const frontRouter = require('./front.router.js')
 const authRouter = require("./auth.router.js")
 const stuRouter = require("./student.router.js")
 const admisRouter = require("./admission.router.js")
 
 
-router.use(frontRouter)
 router.use("/auth", authRouter)
 router.use("/profile", stuRouter)
 router.use("/admission", admisRouter)
